9b16bf8ac3a7654152ea29755bfe00b7f657796c
jeevan-p-gowda <jeevanputtaswamy@gmail.com>
2025-01-07 01:34:34 +0530

    formats

--- src/pageObjects/CommonPage.ts
+++ src/pageObjects/CommonPage.ts
@@ -2,22 +2,22 @@ import { Locator, Page } from '@playwright/test';
 import BasePage from '../fixtures/BasePage';
 
 export default class CommonPage extends BasePage {
-    private readonly page: Page;
-    private readonly homeIcon: Locator;
-    private readonly accountIcon: Locator;
+  private readonly page: Page;
+  private readonly homeIcon: Locator;
+  private readonly accountIcon: Locator;
 
-    constructor(page: Page) {
-        super(page);
-        this.page = page;
-        this.homeIcon = page.locator("//span[.='Home']");
-        this.accountIcon = page.getByRole('link', { name: 'Log in' });
-    }
+  constructor(page: Page) {
+    super(page);
+    this.page = page;
+    this.homeIcon = page.locator("//span[.='Home']");
+    this.accountIcon = page.getByRole('link', { name: 'Log in' });
+  }
 
-    async navigateToProductsPage(): Promise<void> {
-        await this.homeIcon.click();
-    }
+  async navigateToProductsPage(): Promise<void> {
+    await this.homeIcon.click();
+  }
 
-    async navigateToLoginPage(): Promise<void> {
-        await this.accountIcon.click();
-    }
+  async navigateToLoginPage(): Promise<void> {
+    await this.accountIcon.click();
+  }
 }
