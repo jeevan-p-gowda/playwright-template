bbfad4443b26a0e8331b85127dbfa8c713e6df67
jeevan-p-gowda <jeevanputtaswamy@gmail.com>
2025-05-15 01:16:06 +0530

    config: updated with navigate and action timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ dotenv.config({
 export default defineConfig({
   testDir: './tests',
   workers: 1,
-  reporter: [['html', { open: 'never' }], ['./src/utils/WinstonLoggerConfig.ts'], ['junit', { outputFile: 'playwright-report/junit.xml' }]],
+  reporter: [['html', { open: 'never' }], ['junit', { outputFile: 'playwright-report/junit.xml' }], ['./src/utils/WinstonLoggerConfig.ts']],
   use: {
     viewport: { width: 1280, height: 720 },
     browserName: 'chromium',
@@ -18,8 +18,10 @@ export default defineConfig({
     video: 'retain-on-failure',
     trace: 'retain-on-failure',
     storageState: `.auth/${process.env.ENV}-auth.json`,
+    navigationTimeout: 40 * 1000,
+    actionTimeout: 80 * 1000,
   },
   expect: { timeout: 15000 },
-  globalSetup: require.resolve('./src/fixtures/GlobalSetup.ts'),
-  globalTeardown: require.resolve('./src/fixtures/GlobalTearDown.ts'),
+  // globalSetup: require.resolve('./src/fixtures/GlobalSetup.ts'),
+  // globalTeardown: require.resolve('./src/fixtures/GlobalTearDown.ts'),
 });
