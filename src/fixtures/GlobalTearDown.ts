3cb6763c7fd1cde15d04c5541616d64e760d96bd
jeevan-p-gowda <jeevanputtaswamy@gmail.com>
2025-05-28 13:45:45 +0530

    add: logs to global fixture

--- src/fixtures/GlobalTearDown.ts
+++ src/fixtures/GlobalTearDown.ts
@@ -1,6 +1,6 @@
 async function globalTeardown() {
   console.log('--Executing Tear Down--');
-  console.log('--Tear Down Completed--');
+  console.log('--Tear Down Completed ✅--');
 }
 
 export default globalTeardown;
