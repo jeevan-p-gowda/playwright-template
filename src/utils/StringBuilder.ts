ddc50a35780ba687e8d819798c15f10447c70b10
jeevan-p-gowda <jeevanputtaswamy@gmail.com>
2025-05-28 13:44:14 +0530

    refactor: string builder

--- src/utils/StringBuilder.ts
+++ src/utils/StringBuilder.ts
@@ -1,12 +1,13 @@
 export default class StringBuilder {
   constructor() {}
 
-  async emailBuilder(): Promise<string> {
-    const randomPart: string = Math.random().toString(36).substring(2, 15);
-    return randomPart + '@gmail.com';
+  async getRandomCreds(): Promise<{ email: string; password: string }> {
+    const email: string = Math.random().toString(36).substring(2, 15) + '@gmail.com';
+    const password: string = '1234567890';
+    return { email, password };
   }
 
-  async APIBuilder(apiURL: string, endpoint: string, option?: { id: number }): Promise<string> {
+  async apiBuilder(apiURL: string, endpoint: string, option?: { id: number }): Promise<string> {
     let api: string;
     if (option?.id) {
       api = endpoint.replace(/{(.*?)}/, option.id.toString());
