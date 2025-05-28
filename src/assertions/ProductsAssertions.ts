eb3f88c12cad231ffad92e0c2ae465bb15b6c58f
jeevan-p-gowda <jeevanputtaswamy@gmail.com>
2025-05-28 13:43:07 +0530

    refactor: assertion

--- src/assertions/ProductsAssertions.ts
+++ src/assertions/ProductsAssertions.ts
@@ -1,8 +1,10 @@
+import { step } from '../utils/WinstonLoggerConfig';
 import CommonAssertions from './CommonAssertions';
 
 export default class ProductsAssertions extends CommonAssertions {
+  @step()
   async assertSearchResultsHeading(heading: string, options?: { soft: boolean }): Promise<void> {
-    if (options) {
+    if (options?.soft) {
       await this.assertValueEquals(heading, 'Search Results', {
         soft: options.soft,
       });
