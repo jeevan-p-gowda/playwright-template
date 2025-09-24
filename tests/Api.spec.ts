93e6abe702528060d844c70cebbc3adf38d63aec
jeevan-p-gowda <jeevanputtaswamy@gmail.com>
2025-09-24 14:19:24 +0530

    update: integrate CommonApiAssertions into E-Commerce API tests for improved response validation

--- tests/Api.spec.ts
+++ tests/Api.spec.ts
@@ -1,19 +1,24 @@
 import { test as BaseTest } from '@apiFixtures/BaseApiTest';
 import ProductClient from '@apiClients/ProductClient';
+import CommonApiAssertions from '@apiAssertions/CommonApiAssertions';
 
 type fixtures = {
   productClient: ProductClient;
+  commonApiAssertions: CommonApiAssertions;
 };
 
 const test = BaseTest.extend<fixtures>({
   productClient: async ({}, use) => {
     await use(new ProductClient());
   },
+  commonApiAssertions: async ({}, use) => {
+    await use(new CommonApiAssertions());
+  },
 });
 
 test.describe('E-Commerce E2E API tests', () => {
-  test('verify API test', async ({ productClient }) => {
+  test('verify API test', async ({ productClient, commonApiAssertions }) => {
     const response = await productClient.getProductList();
-    console.log('Response:', response);
+    await commonApiAssertions.assertResponseStatusOk(response);
   });
 });
