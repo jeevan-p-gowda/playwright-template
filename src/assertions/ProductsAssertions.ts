ecc98d1912ca34ee4126dbff4916109ca08fa3e3
jeevan-p-gowda <jeevanputtaswamy@gmail.com>
2026-03-09 16:07:37 +0530

    Enhance CommonAssertions and ProductsAssertions

--- src/assertions/ProductsAssertions.ts
+++ src/assertions/ProductsAssertions.ts
@@ -3,13 +3,24 @@ import CommonAssertions from './CommonAssertions';
 
 export default class ProductsAssertions extends CommonAssertions {
   @step()
-  async assertSearchResultsHeading(heading: string, options?: { soft: boolean }): Promise<void> {
+  async assertSearchResultsHeading(heading: any, options?: { soft: boolean }): Promise<void> {
     if (options?.soft) {
       await this.assertValueEquals(heading, 'Search Results', {
         soft: options.soft,
       });
     } else {
       await this.assertValueEquals(heading, 'Search Results');
+    }
+  }
+
+  @step()
+  async assertStatus(status: any, options?: { soft: boolean }): Promise<void> {
+    if (options?.soft) {
+      await this.assertElementTextEquals(status, 'Unauthorized', {
+        soft: options.soft,
+      });
+    } else {
+      await this.assertElementTextEquals(status, 'Unauthorized');
     }
   }
 }
