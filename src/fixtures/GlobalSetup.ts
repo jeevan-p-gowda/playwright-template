0306e7af9217cf1b369c763fcf2a887b7fc5560c
jeevan-p-gowda <jeevanputtaswamy@gmail.com>
2026-03-09 16:06:29 +0530

    added blanks in global setup

--- src/fixtures/GlobalSetup.ts
+++ src/fixtures/GlobalSetup.ts
@@ -7,11 +7,14 @@ async function globalSetup(config: FullConfig) {
   const browser = await chromium.launch();
   const context = await browser.newContext();
   const page = await context.newPage();
+
   console.log('Navigating to base URL...');
   await page.goto(baseURL!);
   const loginPage: LoginPage = new LoginPage(page);
+
   console.log('Logging to the application...');
   await loginPage.login(process.env.EMAIL as string, process.env.PASSWORD as string);
+
   console.log('Setting storage state...');
   await page.context().storageState({ path: storageState as string });
   await browser.close();
